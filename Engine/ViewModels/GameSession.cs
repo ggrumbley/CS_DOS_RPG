6fb889b9fc3bf040c8b56ba799991e3d53773a33
Gary Grumbley <ggrumbley@trackforce.com>
2018-07-16 15:03:36 -0700

    Add location class and world factory

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+        public World CurrentWorld { get; set; }
 
         public GameSession()
         {
-            CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Gary";
-            CurrentPlayer.Gold = 1000000;
-            CurrentPlayer.CharacterClass = "Wizard";
-            CurrentPlayer.HitPoints = 10;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
+            CurrentPlayer = new Player
+            {
+                Name = "Gary",
+                Gold = 1000000,
+                CharacterClass = "Wizard",
+                HitPoints = 10,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
+            
         }
     }
 }
